cfcecc4d4a4a0bdfa215700cfffbc6e754a21e70
Cristhian Valencia <valenciacristhian47@gmail.com>
2016-09-18 02:05:38 -0300

    Lifecycle and showing data

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
 var About = require('./components/about/aboutPage');
+var Author = require('./components/authors/authorPage');
 var Header = require('./components/common/header');
 
 (function(win) {
@@ -12,6 +13,7 @@ var App = React.createClass({
     var Child;
     switch(this.props.route){
       case 'about': Child = About; break;
+      case 'authors': Child = Author; break;
       default: Child = Home;
     }
 
