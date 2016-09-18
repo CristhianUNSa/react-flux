965ec7ce16cac25a2cb9465ad1d848da84757ca6
Cristhian Valencia <valenciacristhian47@gmail.com>
2016-09-18 19:23:29 -0300

    Locations

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -49091,7 +49091,7 @@ var Router = require('react-router');
 
 var routes = require('./routes');
 
-Router.run(routes, function(Handler){
+Router.run(routes, Router.HistoryLocation, function(Handler){
   React.render(React.createElement(Handler, null), document.getElementById('app'));
 });
 
