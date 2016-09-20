20e9933c23a13b9e6906da1615455fc0a3b836eb
Cristhian Valencia <valenciacristhian47@gmail.com>
2016-09-19 22:38:59 -0300

    Validation and PropTypes

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -4,34 +4,57 @@ var React = require('react');
 var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
+var toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
   mixins: [
     Router.Navigation
   ],
-  getInitialState: function(){
+  getInitialState: function () {
     return {
-      author: { id: '', firstName: '', lastName: ''}
+      author: { id: '', firstName: '', lastName: '' },
+      errors: {}
     };
   },
-  setAuthorState: function(event){
+  setAuthorState: function (event) {
     var field = event.target.name;
     var value = event.target.value;
     this.state.author[field] = value;
-    return this.setState({author: this.state.author});
+    return this.setState({ author: this.state.author });
   },
-  saveAuthor: function(event){
+  authorFormIsValid: function () {
+    var formIsValid = true;
+    this.state.errors = {}; //clear any previous error
+    if (this.state.author.firstName.length < 3) {
+      this.state.errors.firstName = 'First name must be at least 3 characters';
+      formIsValid = false;
+    }
+    if (this.state.author.lastName.length < 3) {
+      this.state.errors.lastName = 'Last name must be at least 3 characters';
+      formIsValid = false;
+    }
+
+    this.setState({errors: this.state.errors});
+    return formIsValid;
+  },
+  saveAuthor: function (event) {
     event.preventDefault();
+    if (!this.authorFormIsValid()) {
+      return;
+    }
+
     AuthorApi.saveAuthor(this.state.author);
+    toastr.success('Author Saved');
     this.transitionTo('authors');
   },
-  render: function(){
+  render: function () {
     return (
       <div>
-        <AuthorForm 
+        <AuthorForm
           author={this.state.author}
           onChange={this.setAuthorState}
-          onSave={this.saveAuthor}/>
+          onSave={this.saveAuthor}
+          errors={this.state.errors}/>
       </div>
     );
   }
