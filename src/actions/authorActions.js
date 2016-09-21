92dfd00351dc0e6e156a17e0591871feb266778f
Cristhian Valencia <valenciacristhian47@gmail.com>
2016-09-20 23:58:33 -0300

    Interactions

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -5,7 +5,7 @@ var AuthorApi = require('../api/authorApi');
 var ActionTypes = require('../constants/actionTypes');
 
 var AuthorActions = {
-  createAuthor = function(author){
+  createAuthor: function(author){
     var newAuthor = AuthorApi.saveAuthor(author);
 
     //Hey dispatcher, go tell all the stores that an autor was just created
--- src/stores/authorStore.js
+++ src/stores/authorStore.js
@@ -4,18 +4,40 @@ var Dispatcher = require('../dispatcher/appDispatcher');
 var ActionTypes = require('../constants/actionTypes');
 var EventEmitter = require('events').EventEmitter;
 var assign = require('object-assign');
+var _ = require('lodash');
 var CHANGE_EVENT = 'change';
 
+var _authors = [];
+
 var AuthorStore = assign({}, EventEmitter.prototype, {
-  addChangeListener: function(callback){
+  addChangeListener: function (callback) {
     this.on(CHANGE_EVENT, callback);
   },
 
-  removeChangeListener: function(callback){
+  removeChangeListener: function (callback) {
     this.removeListener(CHANGE_EVENT, callback);
   },
 
-  emitChange: function(){
+  emitChange: function () {
     this.emit(CHANGE_EVENT);
+  },
+
+  getAllAuthors: function(){
+    return _authors;
+  },
+
+  getAuthorById: function(id){
+    return _.find(_authors, {id: id});
   }
 });
+
+Dispatcher.register(function (action) {
+  switch (action.actionType) {
+    case ActionTypes.CREATE_AUTHOR: 
+      _authors.push(action.author);
+      AuthorStore.emitChange();
+      break;
+  }
+});
+
+module.exports = AuthorStore;
